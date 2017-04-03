d5100cb696c6de32bc78e5fab280d686cb9fe90d
Filip Weilid <filip.weilid@gmail.com>
2017-04-03 13:05:18 +0200

    Test client klass

--- src/server/TestServer.java
+++ src/server/TestServer.java
@@ -1,13 +1,7 @@
 package server;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
  
 public class TestServer
 {
