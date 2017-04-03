2d566429ea440283934869c281374637361d84a7
Zozappy <Zozappy>
2017-04-03 12:00:14 +0200

    Send och Recieve klasser

--- src/pcClient/Send.java
+++ src/pcClient/Send.java
@@ -1,5 +1,46 @@
 package pcClient;
 
+import java.io.*;
+import java.net.*;
+
+import javax.swing.JOptionPane;
+
 public class Send {
+	boolean fis = false;
+
+	public boolean button() {
+
+
+		return fis;
+	}
+
+	public void Sender() throws UnknownHostException, IOException {
+
+		Socket s = new Socket("localhost", 4711);
+		PrintWriter sockOut = new PrintWriter(s.getOutputStream());
+		System.out.println("Client connected");
+		if (fis = true) {
+			sockOut.append("Accept");
+		}
+		sockOut.close(); // Flushes the out stream.
+		s.close();
+	}
+
+	public static void main(String[] args) throws IOException, UnknownHostException {
+
+		Send b = new Send();
+		b.button();
+		b.Sender();
+		// Socket s = new Socket("localhost", 4711);
+		// PrintWriter sockOut = new PrintWriter(s.getOutputStream());
+		// System.out.println("Client connected");
+		//
+		// sockOut.append("Accept");
+		//
+		//
+		// sockOut.close(); //Flushes the out stream.
+		// s.close();
+
+	}
 
 }
--- src/server/ReadAndPrint.java
+++ src/server/ReadAndPrint.java
@@ -1,5 +1,44 @@
 package server;
 
+import java.io.*;
+import java.net.*;
+
+/**
+ * Reads Strings from a ServerSocket and prints them to standard out.
+ */
 public class ReadAndPrint {
+    BufferedReader sockIn;
+
+    public ReadAndPrint(Socket s) throws IOException {
+	sockIn = new BufferedReader(new InputStreamReader(s.getInputStream()));
+    }
+
+    public void service() {
+	String msg;
+
+	try {
+	    /* readLine() returns null if the end of the stream i reached. */
+	    while((msg = sockIn.readLine()) != null) { 
+		System.out.println("Server got: " + msg);
+	    }
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+
+    }
+    
+    public static void main(String[] args) throws IOException {
+	ServerSocket ss = new ServerSocket(4711);
+	
+	while(true) {
+	    Socket s = ss.accept(); 
+	    /* A connection is established when accept() returns. */
+	    System.out.println("Connection established from " + 
+			       s.getInetAddress().getHostName());
+	    ReadAndPrint rap = new ReadAndPrint(s);
+	    rap.service();
+	}
+	
+    }
 
 }
