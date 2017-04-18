ed17512748284c3cc3588763940484216a7e4d0a
ViktorKullberg <viktorkullberg1@gmail.com>
2017-04-18 13:18:59 +0200

    e

--- src/server/ServerConnectivity.java
+++ src/server/ServerConnectivity.java
@@ -3,20 +3,29 @@ package server;
 import java.io.*;
 import java.net.*;
 
-
 //SERVERN SOM LYSSNAR PÅ INKOMMANDE TRAFIK OCH ANVÄNDER SERVERCONTROLLER FÖR 
 //ATT VETA VAD SOM SKA SKE
 public class ServerConnectivity {
-	
+
 	private Socket socket;
-	//private ServerGUI gui;
+	// private ServerGUI gui;
 	private ServerController controller;
+	private int port;
 
 	public ServerConnectivity(int port, ServerController controller) {
+		this.port = port;
 		this.controller = controller;
+		System.out.println("Server Started and listening to the port 25000");
+		getConnection();
+	}
+	
+	
+	/*
+	 * Method that listens to the serversocket and executes the action
+	 */
+	public void getConnection() {
 		try {
 			ServerSocket serverSocket = new ServerSocket(port);
-			System.out.println("Server Started and listening to the port 25000");
 			// Server is running always. This is done using this while(true)
 			// loop
 			while (true) {
@@ -26,7 +35,8 @@ public class ServerConnectivity {
 				InputStreamReader isr = new InputStreamReader(is, "UTF-8");
 				BufferedReader br = new BufferedReader(isr);
 				String data = br.readLine();
-				controller.proccesData(data, socket); //serverControllern hanterar datan
+				controller.proccesData(data, socket); // serverControllern
+														// hanterar datan
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -37,5 +47,4 @@ public class ServerConnectivity {
 			}
 		}
 	}
-
 }
